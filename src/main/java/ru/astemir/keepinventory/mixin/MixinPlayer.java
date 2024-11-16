9bd5e213864f369da349b6829dc34a2d5a40ac53
Ksantor <54307321+DrKsantor@users.noreply.github.com>
2024-11-16 01:51:40 +0100

    resolve import issues in main mod classes, configure mod

--- src/main/java/ru/astemir/keepinventory/mixin/MixinPlayer.java
+++ src/main/java/ru/astemir/keepinventory/mixin/MixinPlayer.java
@@ -1,73 +1,67 @@
-/*    */ package ru.astemir.keepinventory.mixin;
-/*    */ 
-/*    */ import java.util.List;
-/*    */ import javax.annotation.Nullable;
-/*    */ import net.minecraft.world.entity.EntityType;
-/*    */ import net.minecraft.world.entity.LivingEntity;
-/*    */ import net.minecraft.world.entity.item.ItemEntity;
-/*    */ import net.minecraft.world.entity.player.Inventory;
-/*    */ import net.minecraft.world.entity.player.Player;
-/*    */ import net.minecraft.world.item.ItemStack;
-/*    */ import net.minecraft.world.item.enchantment.EnchantmentHelper;
-/*    */ import net.minecraft.world.level.GameRules;
-/*    */ import net.minecraft.world.level.Level;
-/*    */ import org.spongepowered.asm.mixin.Mixin;
-/*    */ import org.spongepowered.asm.mixin.Shadow;
-/*    */ import org.spongepowered.asm.mixin.injection.At;
-/*    */ import org.spongepowered.asm.mixin.injection.Inject;
-/*    */ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-/*    */ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
-/*    */ import ru.astemir.keepinventory.KIConfig;
-/*    */ 
-/*    */ @Mixin({Player.class})
-/*    */ public abstract class MixinPlayer extends LivingEntity {
-/*    */   @Shadow
-/*    */   public abstract Inventory m_150109_();
-/*    */   
-/*    */   @Shadow
-/*    */   @Nullable
-/*    */   public abstract ItemEntity m_7197_(ItemStack paramItemStack, boolean paramBoolean1, boolean paramBoolean2);
-/*    */   
-/*    */   protected MixinPlayer(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
-/* 32 */     super(p_20966_, p_20967_);
-/*    */   }
-/*    */   
-/*    */   @Inject(method = {"dropEquipment"}, at = {@At("HEAD")}, cancellable = true)
-/*    */   public void _onDropEquipment(CallbackInfo ci) {
-/* 37 */     boolean keepInventory = ((GameRules.BooleanValue)this.f_19853_.m_46469_().m_46170_(GameRules.f_46133_)).m_46223_();
-/* 38 */     if (!keepInventory && (
-/* 39 */       (Boolean)KIConfig.ENABLED.get()).booleanValue()) {
-/* 40 */       List<? extends Integer> savedSlots = (List<? extends Integer>)KIConfig.KEEPED_SLOTS.get();
-/* 41 */       int containerSize = m_150109_().m_6643_();
-/* 42 */       for (int i = 0; i < containerSize; i++) {
-/* 43 */         ItemStack itemstack = m_150109_().m_8020_(i);
-/* 44 */         if (!savedSlots.contains(Integer.valueOf(i)) && 
-/* 45 */           !itemstack.m_41619_()) {
-/* 46 */           if (EnchantmentHelper.m_44924_(itemstack)) {
-/* 47 */             m_150109_().m_8016_(i);
-/*    */           } else {
-/* 49 */             m_7197_(itemstack, true, false);
-/* 50 */             m_150109_().m_6836_(i, ItemStack.f_41583_);
-/*    */           } 
-/*    */         }
-/*    */       } 
-/*    */       
-/* 55 */       ci.cancel();
-/*    */     } 
-/*    */   }
-/*    */ 
-/*    */   
-/*    */   @Inject(method = {"getExperienceReward"}, at = {@At("HEAD")}, cancellable = true)
-/*    */   public void _onGetExperience(CallbackInfoReturnable<Integer> cir) {
-/* 62 */     boolean keepInventory = ((GameRules.BooleanValue)this.f_19853_.m_46469_().m_46170_(GameRules.f_46133_)).m_46223_();
-/* 63 */     if (!keepInventory && (
-/* 64 */       (Boolean)KIConfig.ENABLED.get()).booleanValue() && ((Boolean)KIConfig.KEEP_EXPERIENCE.get()).booleanValue())
-/* 65 */       cir.setReturnValue(Integer.valueOf(0)); 
-/*    */   }
-/*    */ }
+// package ru.astemir.keepinventory.mixin;
 
+// import java.util.List;
+// import javax.annotation.Nullable;
+// import net.minecraft.world.entity.EntityType;
+// import net.minecraft.world.entity.LivingEntity;
+// import net.minecraft.world.entity.item.ItemEntity;
+// import net.minecraft.world.entity.player.Inventory;
+// import net.minecraft.world.entity.player.Player;
+// import net.minecraft.world.item.ItemStack;
+// import net.minecraft.world.item.enchantment.EnchantmentHelper;
+// import net.minecraft.world.level.GameRules;
+// import net.minecraft.world.level.Level;
+// import org.spongepowered.asm.mixin.Mixin;
+// import org.spongepowered.asm.mixin.Shadow;
+// import org.spongepowered.asm.mixin.injection.At;
+// import org.spongepowered.asm.mixin.injection.Inject;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+// import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+// import ru.astemir.keepinventory.KIConfig;
 
-/* Location:              C:\Users\ksantor\Documents\repo\ConfigurableInventory\ConfigurableKeepInventory-1.19.2-1.11.jar!\ru\astemir\keepinventory\mixin\MixinPlayer.class
- * Java compiler version: 17 (61.0)
- * JD-Core Version:       1.1.3
- */
+//@Mixin({Player.class})
+//public abstract class MixinPlayer extends LivingEntity {
+// /*    */   @Shadow
+// /*    */   public abstract Inventory m_150109_();
+// /*    */   
+// /*    */   @Shadow
+// /*    */   @Nullable
+// /*    */   public abstract ItemEntity m_7197_(ItemStack paramItemStack, boolean paramBoolean1, boolean paramBoolean2);
+// /*    */   
+// /*    */   protected MixinPlayer(EntityType<? extends LivingEntity> p_20966_, Level p_20967_) {
+// /* 32 */     super(p_20966_, p_20967_);
+// /*    */   }
+// /*    */   
+// /*    */   @Inject(method = {"dropEquipment"}, at = {@At("HEAD")}, cancellable = true)
+// /*    */   public void _onDropEquipment(CallbackInfo ci) {
+// /* 37 */     boolean keepInventory = ((GameRules.BooleanValue)this.f_19853_.m_46469_().m_46170_(GameRules.f_46133_)).m_46223_();
+// /* 38 */     if (!keepInventory && (
+// /* 39 */       (Boolean)KIConfig.ENABLED.get()).booleanValue()) {
+// /* 40 */       List<? extends Integer> savedSlots = (List<? extends Integer>)KIConfig.KEEPED_SLOTS.get();
+// /* 41 */       int containerSize = m_150109_().m_6643_();
+// /* 42 */       for (int i = 0; i < containerSize; i++) {
+// /* 43 */         ItemStack itemstack = m_150109_().m_8020_(i);
+// /* 44 */         if (!savedSlots.contains(Integer.valueOf(i)) && 
+// /* 45 */           !itemstack.m_41619_()) {
+// /* 46 */           if (EnchantmentHelper.m_44924_(itemstack)) {
+// /* 47 */             m_150109_().m_8016_(i);
+// /*    */           } else {
+// /* 49 */             m_7197_(itemstack, true, false);
+// /* 50 */             m_150109_().m_6836_(i, ItemStack.f_41583_);
+// /*    */           } 
+// /*    */         }
+// /*    */       } 
+// /*    */       
+// /* 55 */       ci.cancel();
+// /*    */     } 
+// /*    */   }
+// /*    */ 
+// /*    */   
+// /*    */   @Inject(method = {"getExperienceReward"}, at = {@At("HEAD")}, cancellable = true)
+// /*    */   public void _onGetExperience(CallbackInfoReturnable<Integer> cir) {
+// /* 62 */     boolean keepInventory = ((GameRules.BooleanValue)this.f_19853_.m_46469_().m_46170_(GameRules.f_46133_)).m_46223_();
+// /* 63 */     if (!keepInventory && (
+// /* 64 */       (Boolean)KIConfig.ENABLED.get()).booleanValue() && ((Boolean)KIConfig.KEEP_EXPERIENCE.get()).booleanValue())
+// /* 65 */       cir.setReturnValue(Integer.valueOf(0)); 
+// /*    */   }
+//}
--- src/main/java/ru/astemir/keepinventory/mixin/MixinServerPlayer.java
+++ src/main/java/ru/astemir/keepinventory/mixin/MixinServerPlayer.java
@@ -1,62 +1,62 @@
-/*    */ package ru.astemir.keepinventory.mixin;
-/*    */ 
-/*    */ import com.mojang.authlib.GameProfile;
-/*    */ import java.util.Iterator;
-/*    */ import java.util.List;
-/*    */ import net.minecraft.core.BlockPos;
-/*    */ import net.minecraft.server.level.ServerPlayer;
-/*    */ import net.minecraft.world.effect.MobEffectInstance;
-/*    */ import net.minecraft.world.entity.player.Player;
-/*    */ import net.minecraft.world.entity.player.ProfilePublicKey;
-/*    */ import net.minecraft.world.item.ItemStack;
-/*    */ import net.minecraft.world.level.Level;
-/*    */ import org.jetbrains.annotations.Nullable;
-/*    */ import org.spongepowered.asm.mixin.Mixin;
-/*    */ import org.spongepowered.asm.mixin.injection.At;
-/*    */ import org.spongepowered.asm.mixin.injection.Inject;
-/*    */ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-/*    */ import ru.astemir.keepinventory.KIConfig;
-/*    */ 
-/*    */ @Mixin({ServerPlayer.class})
-/*    */ public abstract class MixinServerPlayer extends Player {
-/*    */   public MixinServerPlayer(Level p_219727_, BlockPos p_219728_, float p_219729_, GameProfile p_219730_, @Nullable ProfilePublicKey p_219731_) {
-/* 23 */     super(p_219727_, p_219728_, p_219729_, p_219730_, p_219731_);
-/*    */   }
-/*    */   
-/*    */   @Inject(method = {"restoreFrom"}, at = {@At("TAIL")})
-/*    */   public void _OnRestoreFrom(ServerPlayer player, boolean p_9017_, CallbackInfo ci) {
-/* 28 */     if (!p_9017_ && ((Boolean)KIConfig.ENABLED.get()).booleanValue()) {
-/* 29 */       if (((Boolean)KIConfig.KEEP_EXPERIENCE.get()).booleanValue()) {
-/* 30 */         this.f_36078_ = (int)(player.f_36078_ * ((Double)KIConfig.KEEPED_EXPERIENCE_MODIFIER.get()).doubleValue());
-/* 31 */         this.f_36079_ = player.f_36079_;
-/* 32 */         this.f_36080_ = player.f_36080_;
-/*    */       } 
-/* 34 */       if (((Boolean)KIConfig.KEEP_HUNGER.get()).booleanValue()) {
-/* 35 */         int hungerLevel = (int)(player.m_36324_().m_38702_() * ((Double)KIConfig.KEEPED_HUNGER_MODIFIER.get()).doubleValue());
-/* 36 */         this.f_36097_.m_38705_(Math.max(hungerLevel, ((Integer)KIConfig.KEEPED_HUNGER_MIN_LIMIT.get()).intValue()));
-/*    */       } 
-/* 38 */       if (((Boolean)KIConfig.KEEP_SATURATION.get()).booleanValue()) {
-/* 39 */         this.f_36097_.m_38717_((float)(player.m_36324_().m_38722_() * ((Double)KIConfig.KEEPED_SATURATION_MODIFIER.get()).doubleValue()));
-/*    */       }
-/* 41 */       if (((Boolean)KIConfig.KEEP_POTION_EFFECTS.get()).booleanValue()) {
-/* 42 */         for (MobEffectInstance activeEffect : player.m_21220_()) {
-/* 43 */           m_7292_(activeEffect);
-/*    */         }
-/*    */       }
-/* 46 */       if (((Boolean)KIConfig.KEEP_SCORE.get()).booleanValue()) {
-/* 47 */         m_36397_(player.m_36344_());
-/*    */       }
-/* 49 */       for (Iterator<Integer> iterator = ((List)KIConfig.KEEPED_SLOTS.get()).iterator(); iterator.hasNext(); ) { int slotId = ((Integer)iterator.next()).intValue();
-/* 50 */         ItemStack itemStack = player.m_150109_().m_8020_(slotId);
-/* 51 */         if (itemStack != null)
-/* 52 */           m_150109_().m_6836_(slotId, itemStack);  }
-/*    */     
-/*    */     } 
-/*    */   }
-/*    */ }
+// /*    */ package ru.astemir.keepinventory.mixin;
+// /*    */ 
+// /*    */ import com.mojang.authlib.GameProfile;
+// /*    */ import java.util.Iterator;
+// /*    */ import java.util.List;
+// /*    */ import net.minecraft.core.BlockPos;
+// /*    */ import net.minecraft.server.level.ServerPlayer;
+// /*    */ import net.minecraft.world.effect.MobEffectInstance;
+// /*    */ import net.minecraft.world.entity.player.Player;
+// /*    */ import net.minecraft.world.entity.player.ProfilePublicKey;
+// /*    */ import net.minecraft.world.item.ItemStack;
+// /*    */ import net.minecraft.world.level.Level;
+// /*    */ import org.jetbrains.annotations.Nullable;
+// /*    */ import org.spongepowered.asm.mixin.Mixin;
+// /*    */ import org.spongepowered.asm.mixin.injection.At;
+// /*    */ import org.spongepowered.asm.mixin.injection.Inject;
+// /*    */ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+// /*    */ import ru.astemir.keepinventory.KIConfig;
+// /*    */ 
+// /*    */ @Mixin({ServerPlayer.class})
+// /*    */ public abstract class MixinServerPlayer extends Player {
+// /*    */   public MixinServerPlayer(Level p_219727_, BlockPos p_219728_, float p_219729_, GameProfile p_219730_, @Nullable ProfilePublicKey p_219731_) {
+// /* 23 */     super(p_219727_, p_219728_, p_219729_, p_219730_, p_219731_);
+// /*    */   }
+// /*    */   
+// /*    */   @Inject(method = {"restoreFrom"}, at = {@At("TAIL")})
+// /*    */   public void _OnRestoreFrom(ServerPlayer player, boolean p_9017_, CallbackInfo ci) {
+// /* 28 */     if (!p_9017_ && ((Boolean)KIConfig.ENABLED.get()).booleanValue()) {
+// /* 29 */       if (((Boolean)KIConfig.KEEP_EXPERIENCE.get()).booleanValue()) {
+// /* 30 */         this.f_36078_ = (int)(player.f_36078_ * ((Double)KIConfig.KEEPED_EXPERIENCE_MODIFIER.get()).doubleValue());
+// /* 31 */         this.f_36079_ = player.f_36079_;
+// /* 32 */         this.f_36080_ = player.f_36080_;
+// /*    */       } 
+// /* 34 */       if (((Boolean)KIConfig.KEEP_HUNGER.get()).booleanValue()) {
+// /* 35 */         int hungerLevel = (int)(player.m_36324_().m_38702_() * ((Double)KIConfig.KEEPED_HUNGER_MODIFIER.get()).doubleValue());
+// /* 36 */         this.f_36097_.m_38705_(Math.max(hungerLevel, ((Integer)KIConfig.KEEPED_HUNGER_MIN_LIMIT.get()).intValue()));
+// /*    */       } 
+// /* 38 */       if (((Boolean)KIConfig.KEEP_SATURATION.get()).booleanValue()) {
+// /* 39 */         this.f_36097_.m_38717_((float)(player.m_36324_().m_38722_() * ((Double)KIConfig.KEEPED_SATURATION_MODIFIER.get()).doubleValue()));
+// /*    */       }
+// /* 41 */       if (((Boolean)KIConfig.KEEP_POTION_EFFECTS.get()).booleanValue()) {
+// /* 42 */         for (MobEffectInstance activeEffect : player.m_21220_()) {
+// /* 43 */           m_7292_(activeEffect);
+// /*    */         }
+// /*    */       }
+// /* 46 */       if (((Boolean)KIConfig.KEEP_SCORE.get()).booleanValue()) {
+// /* 47 */         m_36397_(player.m_36344_());
+// /*    */       }
+// /* 49 */       for (Iterator<Integer> iterator = ((List)KIConfig.KEEPED_SLOTS.get()).iterator(); iterator.hasNext(); ) { int slotId = ((Integer)iterator.next()).intValue();
+// /* 50 */         ItemStack itemStack = player.m_150109_().m_8020_(slotId);
+// /* 51 */         if (itemStack != null)
+// /* 52 */           m_150109_().m_6836_(slotId, itemStack);  }
+// /*    */     
+// /*    */     } 
+// /*    */   }
+// /*    */ }
 
 
-/* Location:              C:\Users\ksantor\Documents\repo\ConfigurableInventory\ConfigurableKeepInventory-1.19.2-1.11.jar!\ru\astemir\keepinventory\mixin\MixinServerPlayer.class
- * Java compiler version: 17 (61.0)
- * JD-Core Version:       1.1.3
- */
+// /* Location:              C:\Users\ksantor\Documents\repo\ConfigurableInventory\ConfigurableKeepInventory-1.19.2-1.11.jar!\ru\astemir\keepinventory\mixin\MixinServerPlayer.class
+//  * Java compiler version: 17 (61.0)
+//  * JD-Core Version:       1.1.3
+//  */
